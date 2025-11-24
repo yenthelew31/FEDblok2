d8ea1190326d6c8fd0eab449ccfd8172137b9778
yenthelew31 <lew.yenthe@icloud.com>
2025-11-24 16:53:58 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 const deButton = document.querySelector("header section button");
 const nav = document.querySelector("header nav");
 
-deButton.onclick = () => {
+deButton.onclick = ()=> {
   deButton.classList.toggle("is-open");
   nav.classList.toggle("open");
 };
